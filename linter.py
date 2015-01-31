e8dc8c0e7ddbc9cef706e0c6786cb208d36fede9
Dan Flettre <fletd01@yahoo.com>
2015-01-30 23:13:53 -0600

    git up (git on up)

---
+++ linter.py
@@ -0,0 +1,28 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by Dan Flettre
+# Copyright (c) 2015 Dan Flettre
+#
+# License: MIT
+#
+
+"""This module exports the Standard plugin class."""
+
+from SublimeLinter.lint import NodeLinter
+
+
+class Standard(NodeLinter):
+
+    """Provides an interface to standard."""
+
+    syntax = ('javascript', 'html', 'javascriptnext', 'javascript 6to5')
+    cmd = 'standard'
+    version_args = '--version'
+    version_re = r'(?P<version>\d+\.\d+\.\d+)'
+    version_requirement = '>= 2.3.0'
+    regex = r'^\s.+:(?P<line>\d+):(?P<col>\d+):(?P<message>.+)'
+    selectors = {
+        'html': 'source.js.embedded.html'
+    }
